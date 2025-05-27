db9f91d93e8450efb39a2943ebea68365284e3cd
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-27 20:57:17 +0300

    Added camera activity

--- app/src/main/java/com/yarek/hammockvision/MainActivity.java
+++ app/src/main/java/com/yarek/hammockvision/MainActivity.java
@@ -1,14 +1,25 @@
 package com.yarek.hammockvision;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button runCameraButton;
+    Button calibrateCameraButton;
+    Button configureCameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +29,45 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        runCameraButton = findViewById(R.id.run_button);
+        calibrateCameraButton = findViewById(R.id.calibrate_button);
+        configureCameraButton = findViewById(R.id.configure_button);
+
+        runCameraButton.setOnClickListener(
+                v -> {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(
+                                this, new String[]{Manifest.permission.CAMERA}, 100
+                        );
+                    } else {
+                        goToRunCameraActivity();
+                    }
+                }
+        );
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 101) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                goToRunCameraActivity();
+            } else {
+                Toast.makeText(this, R.string.camera_permission_denied, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void goToRunCameraActivity() {
+        Intent intent = new Intent(this, RunCameraActivity.class);
+        startActivity(intent);
     }
 }
