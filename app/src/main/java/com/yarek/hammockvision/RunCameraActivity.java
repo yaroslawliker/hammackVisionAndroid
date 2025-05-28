9d5916f2aae206412699b99d1bd8fcf0195fdb60
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-28 21:35:19 +0300

    Added camera PreviewView

--- app/src/main/java/com/yarek/hammockvision/RunCameraActivity.java
+++ app/src/main/java/com/yarek/hammockvision/RunCameraActivity.java
@@ -9,15 +9,18 @@ import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
@@ -29,7 +32,7 @@ import java.util.concurrent.Executors;
 public class RunCameraActivity extends AppCompatActivity {
 
 
-    ImageView imageView;
+    PreviewView previewView;
 
     Executor cameraExecutor;
     ProcessCameraProvider cameraProvider;
@@ -38,6 +41,7 @@ public class RunCameraActivity extends AppCompatActivity {
 
 //    ObjectDetector objectDetector;
 
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,19 @@ public class RunCameraActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_run_camera);
 
-        imageView = findViewById(R.id.runCameraView);
+        previewView = findViewById(R.id.runCameraPreview);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -53,64 +69,21 @@ public class RunCameraActivity extends AppCompatActivity {
             return insets;
         });
 
-        startCameraAnalysis();
     }
 
-//    void loadImageDetector() {
-//
-//        ObjectDetectorOptions options = ObjectDetectorOptions.builder()
-//                .setScoreThreshold(0.5f)
-//                .setMaxResults(5)
-//                .build();
-//    }
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
 
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
 
-    void startCameraAnalysis() {
-        cameraExecutor = Executors.newSingleThreadExecutor();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
-                ProcessCameraProvider.getInstance(this);
-
-        cameraProviderFuture.addListener(() -> {
-            try {
-                cameraProvider = cameraProviderFuture.get();
-
-                preview = new Preview.Builder().build();
-
-                cameraSelector = new CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build();
-
-                ImageAnalysis imageAnalysis =
-                        new ImageAnalysis.Builder()
-                                .setTargetResolution(new Size(1280, 720))
-                                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                                .build();
-
-                imageAnalysis.setAnalyzer(cameraExecutor, imageProxy -> {
-
-
-//                    Bitmap bitmap = imageProxyToBitmap(imageProxy); // твоя реалізація
-//                    Bitmap rotatedBitmap = rotateBitmap(bitmap, imageProxy.getImageInfo().getRotationDegrees());
-//
-//                    runObjectDetection(rotatedBitmap); // передай у TensorFlow Lite
-                    imageProxy.close();
-
-                });
-
-                cameraProvider.unbindAll();
-                cameraProvider.bindToLifecycle(
-                        this,
-                        cameraSelector,
-                        preview,
-                        imageAnalysis
-                );
-
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }, ContextCompat.getMainExecutor(this));
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
     }
+
+
+
 }
